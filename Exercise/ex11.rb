18403841ae47571fb863d3b8059de4c65a3f645f
Fuad <fuadulislam@iut-dhaka.edu>
2021-02-10 11:49:40 +0600

    Exercises

---
+++ Exercise/ex11.rb
@@ -0,0 +1,17 @@
+# print "How old are you? "
+# age = gets.chomp
+# print "How tall are you? "
+# height = gets.chomp
+# print "How much do you weigh? "
+# weight = gets.chomp
+#
+# puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+#
+#
+name = gets.chomp.to_i
+puts 'hi' * name
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
---
+++ Exercise/ex12.rb
@@ -0,0 +1,2 @@
+amount = gets.chomp
+puts  (amount.to_f)/10.0
