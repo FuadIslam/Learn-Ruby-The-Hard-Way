88a64edd925bd7ab4916205860db7186cc6db7da
Fuad <fuadulislam@iut-dhaka.edu>
2021-02-11 19:04:21 +0600

    Exercise

---
+++ Exercise/ex29_if.rb
@@ -0,0 +1,13 @@
+people = 20
+cats = 3
+dogs = 15
+
+
+if people < cats
+puts "Too many cats! The world is doomed!"
+puts "hi"
+elsif cats < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
